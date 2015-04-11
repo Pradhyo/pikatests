9a90eb1ac83d46fbd9b92e697731ade349c9727d
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-11 14:16:43 -0400

    Map QuestionsHandler to /questions

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import webapp2
-from handler import MainHandler, AddQuestion
+from handler import MainHandler, AddQuestion, QuestionsHandler
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
 
 app = webapp2.WSGIApplication([('/', MainHandler),
+							   ('/questions', QuestionsHandler),
 							   ('/add_question', AddQuestion)], debug=True)
