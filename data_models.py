a9184061c464e7857a2a1dd815074f13b6931804
Pradhyo Bijja <pradhyo@nyu.edu>
2015-04-11 04:06:46 -0400

    Add render_question so each question renders properly

--- data_models.py
+++ data_models.py
@@ -1,4 +1,13 @@
 from google.appengine.ext import db
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+def render_str(template, **params):
+	t = jinja_env.get_template(template)
+	return t.render(params)
 
 class Question(db.Model):
 	"""Defines each question """
@@ -7,6 +16,11 @@ class Question(db.Model):
 	course = db.StringProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)	
 	user = db.UserProperty(auto_current_user_add = True)
+
+	def render_question(self):
+		#Since we are passing as html, convert new lines to <br> to render new lines properly
+		self._render_text = self.question.replace('\n', '<br>')
+		return render_str("question.html", q = self)
 
 class Answer(db.Model):
 	"""Defines properties of an answer """
